025d97fb05732333ed93dc9c4c72a97c6b3338e9
piotrmadry <piotr.madry@codewise.com>
2018-11-26 13:57:00 -0800

    fix persistance

--- lambda/custom/controllers/persistence.js
+++ lambda/custom/controllers/persistence.js
@@ -2,11 +2,11 @@
 const persistenceAdapter = require('ask-sdk-s3-persistence-adapter');
 // 2. initialize the skill builder with adapter
 const skillBuilder = Alexa.SkillBuilders.custom().withPersistenceAdapter(
-    new persistenceAdapter.S3PersistenceAdapter({bucketName:process.env.S3_PERSISTENCE_BUCKET})
+    new persistenceAdapter.S3PersistenceAdapter({bucketName: process.env.S3_PERSISTENCE_BUCKET})
 );
 
 /**
-var scoreboard = {
+ var scoreboard = {
   players: [
     {color: "yellow", score: 4},
     {color: "blue", score: 3},
@@ -14,25 +14,26 @@ var scoreboard = {
   ],
   round: 1
 }
-**/
+ **/
 
-
+const Persistance = {
 // 3. sample handler to save attributes
-async saveScores(handlerInput, scoreboard){
-    const attributesManager = handlerInput.attributesManager;
-    attributesManager.setPersistentAttributes(s3Attributes);
-    await attributesManager.savePersistentAttributes();
-    return handlerInput;
-},
+    async saveScores(handlerInput, scoreboard) {
+        const attributesManager = handlerInput.attributesManager;
+        attributesManager.setPersistentAttributes(s3Attributes);
+        await attributesManager.savePersistentAttributes();
+        return handlerInput;
+    },
 
 // 4. sample handler to read attributes
-async getScores(handlerInput){
-    const attributesManager = handlerInput.attributesManager;
-    const s3Attributes = await attributesManager.getPersistentAttributes() || {};
-    return scoreboard;
-}
+    async getScores(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const s3Attributes = await attributesManager.getPersistentAttributes() || {};
+        return scoreboard;
+    }
+};
 
 module.exports = {
-  getScores: getScores,
-  saveScores: saveScores
-};
+    getScores: getScores,
+    saveScores: saveScores
+}
